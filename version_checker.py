e099fed71f78edd3cfdc6e2e0dc2bc19b0cbcdb9
aterzgar <igevher@gmail.com>
2021-10-22 23:28:20 +0200

    -checking the version of libs

---
+++ version_checker.py
@@ -0,0 +1,20 @@
+#check the version of libraries
+#Python version
+import sys
+print('Python: {}'.format(sys.version))
+#scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+#numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+#matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+#import sckit-learn
+import sklearn
+print('scikit-learn: {}'.format(sklearn.__version__))
+#pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+
